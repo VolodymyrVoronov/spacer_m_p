6aa71bf0a9ae6aca9a09e0e19f77924ab2e52008
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-06-28 22:10:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## What is it?
+
+Package for getting space for margin or padding.
+
+## Installation
+
+Run `npm i spacer_m_p` or `yarn add spacer_m_p`
+
+## Use
+
+import getSpace from "spacer_m_p";
+
+- Option 1: **getSpace(1, true)** returns **4px** (string);
+- Option 2: **getSpace(1)** returns **4** (number);
+- Option 3: **getSpace()** returns **24px** (string);
+
+## Parameters
+
+The function accepts two parameters, spacer (number) and withPx (boolean);
+
+By default **getSpace()** returns **24px** (string);
+
+#### Spacings, which function may accept and values, which will be returned:
+
+- 1 => 4
+- 2 => 8
+- 3 => 16
+- 4 => 20
+- 5 => 24
+- 6 => 32
+- 7 => 48
+- 8 => 64
+- 9 => 96
+- 10 = > 160
---
+++ index.js
@@ -0,0 +1,56 @@
+const getSpace = (spacer = 5, withPx = false) => {
+  let gap = 0;
+
+  if (spacer >= 1 && spacer <= 10) {
+    switch (spacer) {
+      case 1:
+        gap = withPx ? "4px" : 4;
+        break;
+
+      case 2:
+        gap = withPx ? "8px" : 8;
+        break;
+
+      case 3:
+        gap = withPx ? "16px" : 16;
+        break;
+
+      case 4:
+        gap = withPx ? "20px" : 20;
+        break;
+
+      case 5:
+        gap = withPx ? "24px" : 24;
+        break;
+
+      case 6:
+        gap = withPx ? "32px" : 32;
+        break;
+
+      case 7:
+        gap = withPx ? "48px" : 48;
+        break;
+
+      case 8:
+        gap = withPx ? "64px" : 64;
+        break;
+
+      case 9:
+        gap = withPx ? "96px" : 96;
+        break;
+
+      case 10:
+        gap = withPx ? "160px" : 160;
+        break;
+
+      default:
+        break;
+    }
+
+    return gap;
+  } else {
+    return (gap = withPx ? "24px" : 24);
+  }
+};
+
+export default getSpace;
